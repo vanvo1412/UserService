228e881785bed8a25d3c6bcb446d4dfc8036e597
Van <vanvdse60837@gmail.com>
2019-05-18 18:02:26 +0700

    update models, entities

--- Entities/User.cs
+++ Entities/User.cs
@@ -10,8 +10,12 @@ namespace UserService
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
+        [Required]
         public int Age { get; set; }
+        [MaxLength(50)]
         public string Address { get; set; }
     }
 }
--- UserViewModel.cs
+++ UserViewModel.cs
@@ -5,11 +5,11 @@ namespace UserService
     public class UserViewModel
     {
         [Required]
-        [MaxLength(50)]
+        [StringLength(50)]
         public string Name { get; set; }
         [Required]
         public int Age { get; set; }
-        [MaxLength(50)]
+        [StringLength(50)]
         public string Address { get; set; }
     }
 }
